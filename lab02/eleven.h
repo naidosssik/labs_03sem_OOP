88140d334b83b945051e55154918f41405c79292
Naida <121602055+naidosssik@users.noreply.github.com>
2023-10-07 17:36:15 +0300

    Update eleven.h

--- lab02/eleven.h
+++ lab02/eleven.h
@@ -1,26 +1,40 @@
 #pragma once
 
 #include <iostream>
-#include <vector>
 #include <string>
 
 class Eleven
 {
 public:
     Eleven();
-    Eleven(const std::string &hexadecimal);
-    Eleven(unsigned char singleDigit);
 
-    Eleven operator+(const Eleven &other) const;
-    Eleven operator-(const Eleven &other) const;
-    bool operator==(const Eleven &other) const;
-    bool operator!=(const Eleven &other) const;
+    Eleven(const size_t & n, unsigned char t = 0);
+    Eleven(const std::initializer_list<unsigned char> &t);
+    Eleven(const std::string &t);
+    Eleven(const Eleven &other);
+    Eleven(Eleven &&other) noexcept;
 
-    std::string toHexadecimal() const;
+    size_t size() const noexcept;
+    unsigned char* array() const noexcept;
+
+    //methods
+    bool operator>(const Eleven &t) const;
+    bool operator>=(const Eleven &t) const;
+    bool operator<(const Eleven &t) const;
+    bool operator<=(const Eleven &t) const;
+    bool operator==(const Eleven &t) const; //с 11 с/с
+    bool operator==(const std::string &t) const; //с любой строкой
+    bool operator!=(const Eleven &t) const;
+    bool operator!=(const std::string &t) const; //переполнение
+
+    Eleven operator+(const Eleven &t) const;
+    Eleven operator-(const Eleven &t) const;
+
+    std::ostream& print(std::ostream& os);
+
+    virtual ~Eleven() noexcept;
 
 private:
-    std::vector<unsigned char> digits;
-    void normalize();
-    unsigned char charToHexDigit(char c) const;
-    char hexDigitToChar(unsigned char digit) const;
+    size_t _size;
+    unsigned char *_array;
 };
